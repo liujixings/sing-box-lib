5a53bbf5356e3bbc4039817e89b1af90463cff03
liujixings <36241123+liujixings@users.noreply.github.com>
2026-06-26 03:25:31 +0000

    Libbox Release v1.13.14

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "Libbox",
-      url: "https://github.com/liujixings/sing-box-lib/releases/download/v1.13.13/Libbox.xcframework.zip",
-      checksum: "fd88e17d83ca0340e817a7feab60c5e5095eb69e697f8d9359a2ea1bd7b89572"
+      url: "https://github.com/liujixings/sing-box-lib/releases/download/v1.13.14/Libbox.xcframework.zip",
+      checksum: "5f7f496be982c767b107efba7ed82d854f5d0147f64b31fcded8fe7c17ad3a21"
     )
   ]
 )
